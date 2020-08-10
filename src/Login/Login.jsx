6a660576279690cfd1db94332e02f739a081231c
Sonu kumar <sonukumar03xt@gmail.com>
2020-08-10 15:05:32 +0530

    dummy_auth added with reduxjs

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,41 +1,63 @@
-import React from 'react';
-import './Login.css';
+import React from "react";
+import "./Login.css";
+import { useDispatch } from "react-redux";
+import { userLogin } from "../Store/user/userSlice";
 
 
-const  App = () => {
+
+const App = ({history}) => {
+  const dispatch = useDispatch();
+  let formRef = React.createRef();
+  const Login = (e) => {
+    e.preventDefault();
+    dispatch(userLogin("dsdsd"));
+    history.push("/dashboard");
+  };
+
   return (
     <>
-	
-	<div id="login" class="wrapper fadeInDown ">
-		<div className="company_info my-2">
-			<h2>Wellcome to Brick Industry Managment</h2>
-		</div>
-  <div id="formContent">
-    <div className="my-3">
-      <h3 className="text-white">Login</h3>
-    </div>
-    <form className="mt-5">
-      <input type="text"  class="fadeIn third" name="email_id" placeholder="username or email" /><br/>
-	  <input type="text"  class="fadeIn third" name="password" placeholder="password" /><br/>
-      <select name="" id="">
-      <option value="" >Login As</option>
-        <option value="">Super Admin</option>
-        <option value="">Admin</option>
-        <option value="">Operator</option>
-      </select><br/>
-      <input type="submit" class="fadeIn fourth" value="Log In" />
-    </form>
+      <div id="login" class="wrapper fadeInDown ">
+        <div className="company_info my-2">
+          <h2>Wellcome to Brick Industry Managment</h2>
+        </div>
+        <div id="formContent">
+          <div className="my-3">
+            <h3 className="text-white">Login</h3>
+          </div>
+          <form className="mt-5" ref={node=>formRef=node} onSubmit={Login}>
+            <input
+              type="text"
+              class="fadeIn third"
+              name="email_id"
+              placeholder="username or email"
+            />
+            <br />
+            <input
+              type="text"
+              class="fadeIn third"
+              name="password"
+              placeholder="password"
+            />
+            <br />
+            <select name="" id="">
+              <option value="">Login As</option>
+              <option value="">Super Admin</option>
+              <option value="">Admin</option>
+              <option value="">Operator</option>
+            </select>
+            <br />
+            <input type="submit" class="fadeIn fourth" value="Log In" />
+          </form>
 
-  
-    <div id="formFooter">
-      <a class="underlineHover" href="#">Forgot Password?</a>
-    </div>
-
-  </div>
-</div>
-
-	</>
+          <div id="formFooter">
+            <a class="underlineHover" href="#">
+              Forgot Password?
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
